b7c2bcfcbf02358f0e1ec98699959aa1007f5d51
Nicolas Constant <NicolasConstant@users.noreply.github.com>
2017-10-28 14:46:53 -0400

    clean up

--- test/ReadForeverTests/RfApiTests.cs
+++ test/ReadForeverTests/RfApiTests.cs
@@ -192,13 +192,6 @@ namespace ReadForeverTests
             httpWebRequest.ContentType = "application/json";
             httpWebRequest.Method = "GET";
 
-            //using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
-            //{
-            //    streamWriter.Write(json);
-            //    streamWriter.Flush();
-            //    streamWriter.Close();
-            //}
-
             var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
             using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
             {
